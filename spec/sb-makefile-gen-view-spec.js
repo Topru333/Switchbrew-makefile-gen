2cb4c9014abb76b54690d782499e969527acfe6d
Topru <topru123@gmail.com>
2019-06-28 18:07:18 +0300

    first init

---
+++ spec/sb-makefile-gen-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SbMakefileGenView from '../lib/sb-makefile-gen-view';
+
+describe('SbMakefileGenView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
